ae66336f55f0aa033288bef8d60b89d6e86f3ef4
Inwoo Hwang <hsjkdss228@naver.com>
2023-10-10 15:58:11 +0900

    [Hotfix] 애플리케이션 실행 인사 메시지 수정
    - 수정 전: Hello World!
    - 수정 후: Into the new world!

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -2,7 +2,7 @@ package app;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Into the new world!";
     }
 
     public static void main(String[] args) {
